817578fd1944cfee571345b7bd7f4d59d7a14364
0729singhrahul <0729singhrahul@gmail.com>
2019-04-09 22:48:31 -0500

    Update TestDeck.java

--- src/framework/TestDeck.java
+++ src/framework/TestDeck.java
@@ -1,1 +1,17 @@
+import static org.junit.Assert.*;
 
+import org.junit.Test;
+
+public class TestDeck {
+	@Test
+	public void testAdd() {
+    
+    Deck d= new Deck();
+    Card t = new Card(11, "Diamonds");
+		
+		int numCards = h.getNumCards();
+		
+		assertEquals(numCards, 0);
+		assertNull(h.getCard(0));
+  
+  }
