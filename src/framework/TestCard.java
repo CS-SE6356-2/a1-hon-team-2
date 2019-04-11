c0ee2e0a15a6b3277bbcb9d17eaa826be5dc551d
matthew-paul <matthew.paul.mail@gmail.com>
2019-04-11 03:23:56 -0500

    Comments and additional tests
    Added tests for .equals() as well as commenting what each test is doing

--- src/framework/TestCard.java
+++ src/framework/TestCard.java
@@ -6,21 +6,31 @@ public class TestCard {
 
 	@Test
 	public void testAdd() {
+		// Check default constructor
 		Card t1 = new Card();
 		
 		assertEquals(t1.getNumber(), 0);
 		assertEquals(t1.getSymbol(), "");
 		
+		// Check parameterized constructor
 		Card t2 = new Card(1, "Spades");
 		
 		assertEquals(t2.getNumber(), 1);
 		assertEquals(t2.getSymbol(), "Spades");
 		
+		// Check setNumber and setSymbol functions
 		t1.setNumber(11);
 		t1.setSymbol("Clubs");
 		
 		assertEquals(t1.getNumber(), 11);
 		assertEquals(t1.getSymbol(), "Clubs");
+		
+		// Check overridden .equals() function
+		t1.setNumber(1);
+		t1.setSymbol("Spades");
+		
+		assertEquals(t1, t2);
+		
 	}
 	
 
--- src/framework/TestDeck.java
+++ src/framework/TestDeck.java
@@ -1,4 +1,5 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.io.File;
 
@@ -8,23 +9,23 @@ public class TestDeck {
 	@Test
 	public void testAdd() {
 
+		// Check default constructor
 		Deck d = new Deck();
-
 
 		assertEquals(d.getNumCards(), 0);
 		assertNull(d.getCard(0));
 		
+		// Check addCards function
 		d.addCards(new File("testDeck.txt"));
 		
 		assertEquals(d.getNumCards(), 3);
-		assertEquals(d.getCard(0).getNumber(), 1);
-		assertEquals(d.getCard(0).getSymbol(), "Hearts");
+		assertEquals(d.getCard(0), new Card(1, "Hearts")); // uses overridden .equals to make sure 1st card is correct
 		assertNull(d.getCard(4));
 		
+		// Check parameterized constructor
 		Deck d2 = new Deck(new File("testDeck2.txt"));
 		
 		assertEquals(d2.getNumCards(), 13);
-		assertEquals(d2.getCard(12).getNumber(), 13);
-		assertEquals(d2.getCard(12).getSymbol(), "Diamonds");
+		assertEquals(d2.getCard(12), new Card(13, "Diamonds"));
 	}
 }
